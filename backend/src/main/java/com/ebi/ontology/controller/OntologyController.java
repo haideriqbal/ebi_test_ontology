c2167a16d2efecf0b5f8b65ea49510d326657ba8
Haider Iqbal <haideriqbal1@hotmail.com>
2023-10-12 22:34:26 +0100

    first commit

---
+++ backend/src/main/java/com/ebi/ontology/controller/OntologyController.java
@@ -0,0 +1,33 @@
+package com.ebi.ontology.controller;
+
+import com.ebi.ontology.service.OntologyService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/ontologies")
+public class OntologyController {
+    private static final Logger logger = LogManager.getLogger(OntologyController.class);
+
+    @Autowired
+    OntologyService ontologyService;
+
+    @Autowired
+    private Gson gson;
+
+    @GetMapping
+    public String getAllOntologyIds() {
+        return gson.toJson(ontologyService.getAllOntologyIds());
+    }
+
+    @GetMapping("/{ontologyId}")
+    public String getOntology(@PathVariable String ontologyId) {
+        // get ontology by ID
+        logger.info("Retrieving ontology {}", ontologyId);
+        return gson.toJson(ontologyService.getOntology(ontologyId));
+    }
+}
